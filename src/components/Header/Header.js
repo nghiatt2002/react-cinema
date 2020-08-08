0fb0a190d30800c393f0b324d6d12f73f515fd02
thuan-nvt(MSI) <thuan1819@gmail.com>
2020-08-08 23:15:26 +0700

    Revert "Revert "demo global scss 2""
    This reverts commit 3bad649f8dbeae43e9ba971ed3e4d8acf3255daa.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,15 @@ export default class Header extends Component {
                 <h3>Header h3</h3>
                 <div className="container">
                     <h1>HEADER H1</h1>
+                    <div className="left">
+                        <h2>HEADER H2</h2>
+                        <div className="left-p">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, velit?</p>
+                        </div>
+                    </div>
+                    <div className = "right">
+                        <h2>HEADER H2</h2>
+                    </div>
                 </div>
             </div>
         )
