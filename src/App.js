ca5a98fdc5ba0ed2addf03a27d475dbc22aa0579
nghiatt <nghiatt2002@gmai.com>
2020-08-31 17:15:25 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Header from './components/Header';
 import ListCinema from './components/ListCinema/';
 // import ListCinema from './components/ListCinema2/ListCinema';
+import ListCinema3 from './components/ListCinema3/ListCinema3';
+
 
 function App() {
   return (
     <div className="App">
-      <ListCinema />
+      <ListCinema3 />
     </div>
   );
 }
