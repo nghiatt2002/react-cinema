1c984734d03f1722112690fc7d3c1c5e6b416322
Thanh Tu <thanhtu181293@gmail.com>
2020-09-09 19:06:29 +0700

    carousel menu carousel-form

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,46 +1,51 @@
 import React, { Component } from 'react'
 import carouselStyle from './Carousel.module.scss';
+import CarouselForm from './CarouselForm';
+import Menu from '../Menu/Menu';
 
 export default class Carousel extends Component {
     render() {
         return (
             <div className={carouselStyle.Carousel}>
-                <div id="carouselSlider" className="carousel__Slider carousel slide carousel-fade">
-                    <ol className="container carousel-indicators">
-                        <li data-target="#carouselSlider" data-slide-to={0} className="active" />
-                        <li data-target="#carouselSlider" data-slide-to={1} />
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src="./img/hero-1.jpg" className="d-block w-100" alt="..." />
-                            <div className="container">
-                                <div className="carousel__Slider--Content text-left">
+                <Menu/>
+                <div className="carousel__Main">
+                    <div id="carouselSlider" className="carousel__Slider carousel slide carousel-fade">
+                        <ol className="container carousel-indicators">
+                            <li data-target="#carouselSlider" data-slide-to={0} className="active" />
+                            <li data-target="#carouselSlider" data-slide-to={1} />
+                        </ol>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="./img/hero-1.jpg" className="d-block w-100" alt="..." />
+                                <div className="container carousel__Slider--Content text-left text-light d-md-block">
                                     <p>ACTION, ADVENTURE, FANTASY</p>
-                                    <h2>End of the World: Part II</h2>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, facere.</p>
+                                    <h3><a href="#">End of the World: Part II</a></h3>
+                                    <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum.</p>
+                                    <div className="movieCarousel__play">
+                                        <span className="rounded-circle border border-white p-3 text-white">PG</span>
+                                        <button className="p-3 text-white">
+                                            <i className="fa fa-play" /><span>PLAY TRAILER</span>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className="carousel__Slider--Play">
-                                    <button><i className="fa fa-play" /></button>
-                                </div>
-                                <form action="" className="carousel__Slider--Form"></form>
                             </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="./img/hero-2.jpg" className="d-block w-100" alt="..." />
-                            <div className="container carousel-caption d-none d-md-block">
-                                <p>ACTION, ADVENTURE, FANTASY</p>
-                                <h2>End of the World: Part II</h2>
-                                <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum.</p>
-                                <div className="movieCarousel__play">
-                                    <span className="rounded-circle border border-white p-3 text-white">pg</span>
-                                    <button className="p-3 text-white">
-                                    <i className="fa fa-play" />PLAY TRAILER
-                                    </button>
-                                    <div className="movieCarousel__overlay" />
+                            <div className="carousel-item">
+                                <img src="./img/hero-2.jpg" className="d-block w-100" alt="..." />
+                                <div className="container carousel__Slider--Content text-left text-light d-md-block">
+                                    <p>ACTION, ADVENTURE, FANTASY</p>
+                                    <h3><a href="#">End of the World: Part II</a></h3>
+                                    <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum.</p>
+                                    <div className="movieCarousel__play">
+                                        <span className="rounded-circle border border-white p-3 text-white">PG</span>
+                                        <button className="p-3 text-white">
+                                            <i className="fa fa-play" /><span>PLAY TRAILER</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <CarouselForm/>
                 </div>
             </div>
         )
