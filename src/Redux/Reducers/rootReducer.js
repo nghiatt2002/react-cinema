52c9cd03265ccb465e7f5196372e1d4bde18fbed
TRAN TRONG NGHIA <nghiatt2002@gmail.com>
2020-09-09 19:32:36 +0700

    fix folder

--- src/Redux/Reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { MovieShowTimeReducer } from './MovieShowTimeReducer'
-
-const rootReducer = combineReducers({
-    MovieShowTimeReducer
-});
-
-export default rootReducer;
