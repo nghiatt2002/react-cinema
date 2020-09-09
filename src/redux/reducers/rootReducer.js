396be3ca5eaced9ec978f457dd53343bdfdf0123
thuan-nvt(MSI) <thuan1819@gmail.com>
2020-09-09 19:28:02 +0700

    delete folder

--- src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import FilmSectionReducers from './FilmSectionReducers'
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-    FilmSectionReducers
-});
-
-export default rootReducer;
